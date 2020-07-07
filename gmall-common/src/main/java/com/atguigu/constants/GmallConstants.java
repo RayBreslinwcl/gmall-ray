6a46ad56fa8641d40d316c4524db20ea660f8653
Raywcl <2728928100@qq.com>
2020-07-08 01:22:07 +0800

    1.模拟日志生产 （1）在common模块 （2）LogJsonController修改备注 （3）修改kafka服务ip文件

--- gmall-common/src/main/java/com/atguigu/constants/GmallConstants.java
+++ gmall-common/src/main/java/com/atguigu/constants/GmallConstants.java
@@ -3,12 +3,25 @@ package com.atguigu.constants;
 public class GmallConstants {
 
     //Kafka主题：启动日志主题
-    public static final String GMALL_START_TOPIC = "gmall_start";
+    public static final String GMALL_START_TOPIC = "GMALL_START";
 
     //Kafka主题：事件日志主题
-    public static final String GMALL_EVENT_TOPIC = "gmall_event";
+    public static final String GMALL_EVENT_TOPIC = "GMALL_EVENT";
 
     //Kafka主题：订单数据主题
-    public static final String GMALL_ORDER_TOPIC = "gmall_order_info";
+    public static final String GMALL_ORDER_TOPIC = "GMALL_ORDER_INFO";
 
+    //============================================================
+    //Kafka主题：启动日志主题
+    public static final String KAFKA_TOPIC_STARTUP="GMALL_STARTUP";
+    //Kafka主题：事件日志主题
+    public static final String KAFKA_TOPIC_EVENT="GMALL_EVENT";
+
+    public static final String KAFKA_TOPIC_NEW_ORDER="GMALL_NEW_ORDER";
+    public static final String KAFKA_TOPIC_ORDER_DETAIL="GMALL_ORDER_DETAIL";
+
+    public static final String ES_INDEX_DAU="gmall2019_dau";
+    public static final String ES_INDEX_NEW_MID="gmall2019_new_mid";
+    public static final String ES_INDEX_NEW_ORDER="gmall2019_new_order";
+    public static final String ES_INDEX_SALE_DETAIL="gmall2019_sale_detail";
 }
--- gmall-logger/src/main/java/com/atguigu/gmalllogger/controller/LoggerController.java
+++ gmall-logger/src/main/java/com/atguigu/gmalllogger/controller/LoggerController.java
@@ -32,15 +32,15 @@ public class LoggerController {
     @PostMapping("log")
     public String logger(@RequestParam("logString") String logStr) {
 
-        //添加时间戳
+        //0 添加时间戳
         JSONObject jsonObject = JSON.parseObject(logStr);
         jsonObject.put("ts", System.currentTimeMillis());
 
-        //打印到控制台并输出至文件
+        //1 打印到控制台并输出至文件
         String tsLogStr = jsonObject.toString();
         log.info(tsLogStr);
 
-        //发送至Kafka，判断发送至哪个主题
+        //2 发送至Kafka，判断发送至哪个主题
         if ("startup".equals(jsonObject.getString("type"))) {
             kafkaTemplate.send(GmallConstants.GMALL_START_TOPIC, tsLogStr);
         } else {
